4e2ed83d149d24fdeb90350fc9ec3487414a1707
Aaron Curtis <126215288+Cheeseboy-83@users.noreply.github.com>
2024-09-17 11:56:30 -0700

    Create main.tf

---
+++ terraform/tutorials/modules-basics/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.109"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id     = "783a2d1e-205e-40e1-a827-19bfd1d86396"
+  storage_use_azuread = true
+}
