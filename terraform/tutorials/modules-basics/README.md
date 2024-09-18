842963fcd3f45d5d2e3db5c554d765a498096f57
Aaron Curtis <aaron.curtis@gatesfoundation.org>
2024-09-18 08:43:15 -0700

    update

---
+++ terraform/tutorials/modules-basics/README.md
@@ -0,0 +1,3 @@
+# Module Basics
+
+This tutorial is designed to teach the basics of deploying Terraform resouces using modules hosted in a Terraform Cloud private registry. It deploys a single resource group.
---
+++ terraform/tutorials/modules-dynamic/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.109"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "783a2d1e-205e-40e1-a827-19bfd1d86396"
+}
+
+module "resource_group" {
+  source   = "app.terraform.io/cheeseboy/resource-group/azurerm"
+  version  = "1.0.11"
+  for_each = { for key, value in var.var.resource_groups : key => value }
+
+  name     = each.value.name
+  location = each.value.location
+  tags     = merge(var.required_tags, each.value.tags)
+}
